9c361f43f29972f601c957a52bc26137a4f9c1fc
Dimitri Walters <me@dimitriwalters.com>
2014-11-03 21:50:31 -0500

    Refactor code

--- scripts/app.js
+++ scripts/app.js
@@ -1,58 +1,41 @@
 /** @jsx React.DOM */
 
-var MenuExample = React.createClass({displayName: 'MenuExample',
-
+var Tabs = React.createClass({
   getInitialState: function () {
-    return { focused: 0 };
+    return {
+      active: 0
+    };
   },
 
-  clicked: function (index) {
-
-    // The click handler will update the state with
-    // the index of the focused menu entry
-
-    this.setState({focused: index});
+  changeTab: function (index) {
+    this.setState({
+      active: index
+    });
   },
 
-  render: function() {
-
-    // Here we will read the items property, which was passed
-    // as an attribute when the component was created
-
+  render: function () {
     var self = this;
 
-    // The map method will loop over the array of menu entries,
-    // and will return a new array with <li> elements.
+    return React.DOM.div(null,
+      React.DOM.ul(null, this.props.tabs.map(function (tab, index) {
+        var style = '';
 
-    return (
-      React.DOM.div(null,
-        React.DOM.ul(null,  this.props.items.map(function(m, index){
+        if (self.state.active === index) {
+          style = 'active';
+        }
 
-          var style = '';
+        return React.DOM.li({
+          className: style,
+          onClick: self.changeTab.bind(self, index)
+        }, tab);
+      })),
 
-          if(self.state.focused == index){
-            style = 'focused';
-          }
-
-          // Notice the use of the bind() method. It makes the
-          // index available to the clicked function:
-
-          return React.DOM.li({className: style, onClick: self.clicked.bind(self, index)}, m);
-
-        })
-
-        ),
-
-        React.DOM.p(null, "Selected: ", this.props.items[this.state.focused])
-      )
+      React.DOM.p(null, 'Selected: ', this.props.tabs[this.state.active])
     );
-
   }
 });
 
-// Render the menu component on the page, and pass an array with menu options
-
 React.renderComponent(
-  MenuExample({items: ['Home', 'Services', 'About', 'Contact us'] }),
+  Tabs({tabs: ['Home', 'Services', 'About', 'Contact us']}),
   document.body
 );
